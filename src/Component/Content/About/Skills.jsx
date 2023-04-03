44a8dc244835baa79f70095000701422bca73ab0
VickyyPrass <vickygalih31@gmail.com>
2023-04-04 02:26:17 +0700

    edit app.js

--- src/Component/Content/About/Skills.jsx
+++ src/Component/Content/About/Skills.jsx
@@ -1,13 +1,4 @@
 import React from "react";
-import htmlLogo from "../../images/html.png";
-import cssLogo from "../../images/css.png";
-import jsLogo from "../../images/javascript.png";
-import sassLogo from "../../images/sass.png";
-import nodeLogo from "../../images/node.png";
-import reactLogo from "../../images/react.png";
-import expressLogo from "../../images/express.png";
-import mongoLogo from "../../images/mongo.png";
-import mysqlLogo from "../../images/mysql.png";
 
 export default class Skills extends React.Component {
     render() {
@@ -15,35 +6,7 @@ export default class Skills extends React.Component {
             <div className="skills">
                 <h3>Skills</h3>
                 <div className="line"></div>
-                <div className="skill-area mt-3">
-                    <div className="item">
-                        <img src={htmlLogo} alt="" />
-                    </div>
-                    <div className="item">
-                        <img src={cssLogo} alt="" />
-                    </div>
-                    <div className="item">
-                        <img src={jsLogo} alt="" />
-                    </div>
-                    <div className="item">
-                        <img src={sassLogo} alt="" />
-                    </div>
-                    <div className="item">
-                        <img src={nodeLogo} alt="" />
-                    </div>
-                    <div className="item">
-                        <img src={reactLogo} alt="" />
-                    </div>
-                    <div className="item">
-                        <img src={expressLogo} alt="" />
-                    </div>
-                    <div className="item">
-                        <img src={mongoLogo} alt="" />
-                    </div>
-                    <div className="item">
-                        <img src={mysqlLogo} alt="" />
-                    </div>
-                </div>
+                <div className="skill-area mt-3"></div>
             </div>
         );
     }
