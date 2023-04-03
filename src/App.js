ec30b3f60078a7b7f1b71583f84adb6222a3e137
VickyyPrass <vickygalih31@gmail.com>
2023-04-04 01:37:46 +0700

    fix bug app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./Component/Content/About";
 import MenuNavbar from "./Component/Navbar/Index";
-import Hero from "./Component/Content/Hero/Index";
+import Hero from "./Component/Content/Hero/Hero";
 
 function App() {
     return (
